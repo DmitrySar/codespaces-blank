8bd632e6cf08469f4c38060984a0b1903ed4c17f
DMITRIY YUSHIN <49056603+DmitrySar@users.noreply.github.com>
2022-11-25 20:18:14 +0000

    add example code

--- src/main/java/ru/evolenta/course/Main.java
+++ src/main/java/ru/evolenta/course/Main.java
@@ -1,7 +1,11 @@
 package ru.evolenta.course;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String text = in.nextLine();
+        System.out.println(text.length());
     }
 }
